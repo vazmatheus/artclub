a36a3a3f345549318fc458ca7b5242075e3a2865
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-02-16 17:13:28 -0300

    feat: implementa rotas do router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Tá rolando!</h1>
-    </div>
-  );
-}
+// import React, { useState } from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={ Home } />
+    {/* <Route exact path="/carteira" component={ wallet } /> */ }
+  </Switch>
+);
 
 export default App;
