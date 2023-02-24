f1013c65225e406cafe6c2183a864c363cebdeab
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-02-24 15:34:13 -0300

    style: cria componente de botão

--- front-end/src/styles/styles.js
+++ front-end/src/styles/styles.js
@@ -8,10 +8,22 @@ const pixelToRem = (...values) => {
 
 export const Title = styled.h1`
   font-size: ${pixelToRem(64)};
-  font-weight: 300;
+  font-weight: 400;
 `
 
 export const Paragraph = styled.p`
   /* color: var(--space); */
-  font-size: ${pixelToRem(28)};
+  font-size: ${pixelToRem(32)};
+  font-weight: 300;
+
+`
+
+export const Button = styled.button`
+  background-color: var(--button);
+  color: var(--textWith);
+  width: ${pixelToRem(264)};
+  height: ${pixelToRem(62)};
+  border: none;
+  border-radius: ${pixelToRem(6)};
+  font-size: ${pixelToRem(32)}
 `
