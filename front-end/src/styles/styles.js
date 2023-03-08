62657272fd2c6206330cf7412ffcae0e2ebafeea
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-03-08 15:54:51 -0300

    style: pequenas alteracoes no css

--- front-end/src/styles/styles.js
+++ front-end/src/styles/styles.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
-
-const pixelToRem = (...values) => {
-  return values
-    .reduce((acc, current) => (acc += current / 16 + `rem `), "")
-    .trim();
-}
+import pixelToRem from '../utils/pxToRem';
 
 export const Title = styled.h1`
   font-size: ${pixelToRem(64)};
@@ -15,7 +10,6 @@ export const Paragraph = styled.p`
   /* color: var(--space); */
   font-size: ${pixelToRem(32)};
   font-weight: 300;
-
 `
 
 export const Button = styled.button`
