435b293ad1636c2d0327f40e7b3bec5682053143
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-03-08 15:53:02 -0300

    feat: implementa pagina de appointment

--- front-end/src/pages/Appointment/index.js
+++ front-end/src/pages/Appointment/index.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import { Main, Form, Title, DivFlex, Input, Select } from './styles'
+import Navbar from '../../components/Navbar'
+import maskPhone from '../../utils/maskPhone'
+
+const values = ['a', 'b', 'c', 'd', 'e']
 
 const appointment = () => {
   return (
-    <div>appointment</div>
+    <>
+      <Navbar />
+      <Main>
+        <Title>agendamento</Title>
+        <Form>
+          <DivFlex>
+            <label htmlFor="name">Nome
+              <Input type="text" id="name" />
+            </label>
+
+            <label htmlFor="lastName">Sobrenome
+              <Input type="text" id="lastName" />
+            </label>
+          </DivFlex>
+
+          <label htmlFor="phone">Telefone
+            <Input type="tel" id="phone" onKeyDown={ maskPhone }
+              placeholder="(31)99999-9999" />
+          </label>
+
+          <label htmlFor="email">E-mail
+            <Input type="email" id="email"
+              placeholder="exemplo@email.com" />
+          </label>
+
+          <DivFlex>
+            <label htmlFor="date">Date
+              <Input type="date" name="date" id="date" />
+            </label>
+
+            <label htmlFor="appt"> Que horas?
+              <Select>
+                { values.map((value, i) =>
+                  <option key={ i }>{ value }</option>
+                ) }
+              </Select>
+            </label>
+          </DivFlex>
+
+          <input type="submit" value="Agendar" />
+        </Form>
+      </Main>
+    </>
   )
 }
 
---
+++ front-end/src/pages/Appointment/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import Background from '../../assets/img/background_appointment_small.jpg'
+import pixelToRem from '../../utils/pxToRem';
+
+export const Main = styled.div`
+  background: url(${Background});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  height: calc(100vh - 56px);
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: ${pixelToRem(40)};
+  padding-top: ${pixelToRem(40)};
+  color: #f1f1f1;
+  text-shadow: 3px 3px #000000d5;
+  display: flex;
+  justify-content: center;
+
+  :before {
+    content: "";
+    display: block;
+    width: 200px;
+    height: 5px;
+    background: #f1f1f1;
+    top: 22%;
+    position: absolute;
+    text-align: center;
+    box-shadow: 3px 3px #000000d5;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 30%;
+  background: #898a93da;
+  padding: 20px 10px;
+  border-radius: ${pixelToRem(10)};
+  width: 95%;
+  gap: ${pixelToRem(10)};
+
+  input[type=submit]{
+    background-color: #433633;
+    color: #f1f1f1;
+    border: none;
+    border-radius: ${pixelToRem(5)};
+    font-size: ${pixelToRem(20)};
+    margin-top: ${pixelToRem(20)};
+    padding: ${pixelToRem(10, 0)};
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+`
+
+export const DivFlex = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  label {
+    width: 45%;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between */
+  }
+`
+
+export const Input = styled.input`
+  border: none;
+  border-radius: ${pixelToRem(5)};
+  outline: none;
+  padding-left: 10px;
+  width: 100%;
+`
+
+export const Select = styled.select`
+    border: none;
+    border-radius: ${pixelToRem(5)};
+    outline: none;
+    padding-left: 10px;
+    height: 100%;
+
+  option {
+
+  }
+`;
